49a49dcacd9c7204fc96359164d0736e9a34646c
Vlad <Vlalam010413@student.jenseneducation.se>
2026-06-04 15:21:05 +0200

    added task 5 to mainbranch

---
+++ ChatGPT-5.js
@@ -0,0 +1,18 @@
+const tal = [5,10,15]
+
+
+function genomsnitt() {
+    if (tal.length === 0) {
+        console.log('ingen data')
+    } else {
+        let sum = 0
+        for(let i=0; i<tal.length; i++) {
+            sum = sum + tal[i]
+        }
+        let average = sum/tal.length
+        console.log(average)
+    }
+}
+
+genomsnitt()
+genomsnitt()
